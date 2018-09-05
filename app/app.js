77cebd5075eb86e4dd77f56b675e3b54b8213d10
ktos <ktos@ktos.ktos>
2018-09-05 15:14:06 +0200

    Add some code smells :)

--- app/app.js
+++ app/app.js
@@ -3,4 +3,11 @@ const app = express()
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
+var detectUselessVariable = "This variable is unnecessary! SonarQube should detect that.";
+
+if(true == false)
+{
+  // Dead code
+}
+
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
